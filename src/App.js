89e360243a2ebc166f3dffc2fba5b8582f34121c
Oluwatobi-PM <rajioluwatobi3@gmail.com>
2022-06-01 15:25:21 +0100

    first work done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+// function App () {
+//     const title = 'Blog Post'
+//     const body = 'This is my blog post'
+//     const comments = [
+//         {id: 1, text: 'Comment One'},
+//         {id: 2, text: 'Comment Two'},
+//         {id: 3, text: 'Comment Three'},
+//     ]
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//     const loading = false
+//     const showComments = true
+
+//     if(loading) return <h1>Loading...</h1>
+
+//     const commentBlock = 
+//     <div className="comments">
+//         <h3> Comments ({comments.length})</h3>
+//         <ul>
+//         {comments.map((comments, index) => (
+//             <li key={index}>{comments.text}</li>
+//         ) )}
+//         </ul>
+//     </div>
+
+//     return (
+//     <div className="container">
+//         <h1>{title}</h1>
+//         <p>{body}</p>
+//         {showComments && commentBlock}
+        
+//     </div>
+//     )
+// }
+
+function App () {
+    return (
+        <div className="container">
+            <h1>My App</h1>
+        </div>
+    )
 }
-
-export default App;
+export default App
